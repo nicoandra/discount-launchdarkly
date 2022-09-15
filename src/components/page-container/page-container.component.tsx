62d5a3f36bd80edad234278393b94c5f7434d513
Peter Graham <peterghm@gmail.com>
2022-09-15 13:13:01 -0400

    better font

--- src/components/page-container/page-container.component.tsx
+++ src/components/page-container/page-container.component.tsx
@@ -6,7 +6,7 @@ export const PageContainer = ({ children }: React.PropsWithChildren) => {
     <Container maxW="900px" paddingTop="5" paddingBottom="10">
       <ColorModeToggle />
       <Heading paddingTop="3" fontSize="3xl" textAlign="center">
-        launchdarkly scuffed
+        LaunchDarkly Scuffed
       </Heading>
       <Center>
         <Text
@@ -17,7 +17,7 @@ export const PageContainer = ({ children }: React.PropsWithChildren) => {
           maxW="md"
           alignSelf="center"
         >
-          use launchdarkly via api token 🤐
+          Use launchdarkly via api token 🤐
         </Text>
       </Center>
       {children}
--- src/theme.ts
+++ src/theme.ts
@@ -4,12 +4,12 @@ import { mode, StyleFunctionProps } from '@chakra-ui/theme-tools';
 export const theme = extendTheme({
   initialColorMode: 'dark',
   useSystemColorMode: false,
+  fonts: {
+    heading: `Inconsolata, monospace`,
+    body: `Inconsolata, monospace`,
+  },
   styles: {
     global: (props: StyleFunctionProps) => ({
-      fonts: {
-        heading: `'Source Code Pro', monospace`,
-        body: `'Source Code Pro', monospace`,
-      },
       body: {
         color: mode('gray.800', 'whiteAlpha.900')(props),
         bg: mode('gray.50', 'gray.800')(props),
